ce9032bcb4280cc91c046a60bf469f7bbc005945
boredom101 <ymnewmark@gmail.com>
2020-03-09 10:09:51 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -10,8 +10,7 @@ import { monitor } from '@colyseus/monitor';
 import { ChatRoom } from "./rooms/01-chat-room";
 import { StateHandlerRoom } from "./rooms/02-state-handler";
 import { AuthRoom } from "./rooms/03-auth";
-import { EnergyRoom } from "./rooms/04-energy";
-import { dash } from "./rooms/04-energy";
+import { energy } from "./rooms/04-energy";
 
 const port = Number(process.env.PORT || 2567) + Number(process.env.NODE_APP_INSTANCE || 0);
 const app = express();
@@ -40,8 +39,10 @@ gameServer.define("state_handler", StateHandlerRoom);
 // Register StateHandlerRoom as "state_handler"
 gameServer.define("auth", AuthRoom);
 
-gameServer.define("energy", EnergyRoom);
-app.use('/dash', dash());
+var arr = energy();
+
+gameServer.define("energy", arr[1]);
+app.use('/dash', arr[0]);
 
 app.use('/', express.static(path.join(__dirname, "static")));
 app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
